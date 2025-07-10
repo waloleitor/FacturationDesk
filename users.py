86bbffd33c8b30d38a318204a5c8afe2a3990179
waloleitor <tsarciatroch@gmail.com>
2025-07-10 19:03:29 +0200

    Estructura base del proyecto creada

---
+++ users.py
@@ -0,0 +1,5 @@
+USERS = {
+    "admin": {"password": "8856LLR@dacia", "rol": "administrador" },
+    "coordinador": {"password": "3555LTG", "rol": "coordinador" },
+    "ayudante": {"password": "64323MHZ", "rol": "ayudante" }
+}
