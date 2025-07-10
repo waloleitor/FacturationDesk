1384e64b4950bc1050f0ae7c608aa3c08825f32f
waloleitor <tsarciatroch@gmail.com>
2025-07-10 21:16:57 +0200

    Ocultar contraseña con echoMode en login

--- frontend/login_window.py
+++ frontend/login_window.py
@@ -15,44 +15,37 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGroupBox, QLabel, QLineEdit,
-    QMainWindow, QMenuBar, QPushButton, QSizePolicy,
-    QStatusBar, QWidget)
+from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QMainWindow,
+    QMenuBar, QPushButton, QSizePolicy, QStatusBar,
+    QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(615, 479)
+        MainWindow.resize(297, 309)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.titleLabel = QLabel(self.centralwidget)
         self.titleLabel.setObjectName(u"titleLabel")
-        self.titleLabel.setGeometry(QRect(180, 70, 231, 111))
+        self.titleLabel.setGeometry(QRect(30, 0, 231, 111))
         self.usernameImput = QLineEdit(self.centralwidget)
         self.usernameImput.setObjectName(u"usernameImput")
-        self.usernameImput.setGeometry(QRect(210, 200, 171, 22))
+        self.usernameImput.setGeometry(QRect(60, 90, 171, 22))
         self.passwordInput = QLineEdit(self.centralwidget)
         self.passwordInput.setObjectName(u"passwordInput")
-        self.passwordInput.setGeometry(QRect(210, 240, 171, 22))
+        self.passwordInput.setGeometry(QRect(60, 130, 171, 22))
+        self.passwordInput.setEchoMode(QLineEdit.EchoMode.Password)
         self.loginButton = QPushButton(self.centralwidget)
         self.loginButton.setObjectName(u"loginButton")
-        self.loginButton.setGeometry(QRect(250, 280, 81, 24))
-        self.groupBox = QGroupBox(self.centralwidget)
-        self.groupBox.setObjectName(u"groupBox")
-        self.groupBox.setGeometry(QRect(60, 50, 461, 331))
-        self.messageLabel = QLabel(self.groupBox)
-        self.messageLabel.setObjectName(u"messageLabel")
-        self.messageLabel.setGeometry(QRect(90, 290, 281, 16))
+        self.loginButton.setGeometry(QRect(100, 170, 91, 24))
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(120, 240, 171, 20))
         MainWindow.setCentralWidget(self.centralwidget)
-        self.groupBox.raise_()
-        self.titleLabel.raise_()
-        self.usernameImput.raise_()
-        self.passwordInput.raise_()
-        self.loginButton.raise_()
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 615, 22))
+        self.menubar.setGeometry(QRect(0, 0, 297, 22))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -65,11 +58,10 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.titleLabel.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p align=\"center\"><span style=\" font-size:16pt; font-weight:700; text-decoration: underline; color:#ff0000;\">Facturarion-Desk</span></p></body></html>", None))
+        self.titleLabel.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p align=\"center\"><span style=\" font-size:16pt; font-weight:700; text-decoration: underline; color:#ff0000;\">Facturation-Desk</span></p></body></html>", None))
         self.usernameImput.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Usuario", None))
         self.passwordInput.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Contrase\u00f1a", None))
         self.loginButton.setText(QCoreApplication.translate("MainWindow", u"Iniciar sesi\u00f3n", None))
-        self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"inicio_sesion", None))
-        self.messageLabel.setText("")
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Desarrollado por : Tom\u00e1s Sarciat", None))
     # retranslateUi
 
